c4f9ccd9160a187be7e3782438e5575e7ba0b660
krau <krau39@outlook.com>
2023-03-17 20:14:03 +0800

    中文变量名!

--- src/filters.py
+++ src/filters.py
@@ -7,16 +7,16 @@ utils = Utils()
 config = utils.read_config('config.yml')
 botname = config.get('botname', 'Kmua')
 
-regex_setu = "涩图|色图|色色|涩涩"
-regex_ohayo = "醒了|早起|下床|刚醒|睡醒了|早安|早上好|睡过了|哦哈哟|ohayo|醒力"
-regex_sleep = "睡了|眠了|wanan|晚安|哦呀斯密|oyasimi|睡力"
-regex_niubi = "bot|机器人|智械危机|Bot"
-regex_yinyu = "[a-zA-Z]"
-regex_noyinyu = f"[^krau|{botname}|{botname.lower()}|{botname.upper()}|@krauisme|@acherkrau]"
-regex_at = f"{botname}|{botname.lower()}|{botname.upper()}"
-regex_mcmod = r"www.mcmod.cn/class"
-regex_into_dict = "入典|史官"
-weni_words = utils.load_words('weni')
+色图正则 = "涩图|色图|色色|涩涩"
+早安正则 = "醒了|早起|下床|刚醒|睡醒了|早安|早上好|睡过了|哦哈哟|ohayo|醒力"
+晚安正则 = "睡了|眠了|wanan|晚安|哦呀斯密|oyasimi|睡力"
+牛逼话正则 = "bot|机器人|智械危机|Bot"
+淫语正则 = "[a-zA-Z]"
+不要淫语正则 = f"[^krau|{botname}|{botname.lower()}|{botname.upper()}|@krauisme|@acherkrau]"
+艾特正则 = f"{botname}|{botname.lower()}|{botname.upper()}"
+mcmod正则 = r"www.mcmod.cn/class"
+入典正则 = "入典|史官"
+文爱关键词 = utils.load_words('weni')
 
 
 class FilterWeniKey(MessageFilter):
@@ -24,7 +24,7 @@ class FilterWeniKey(MessageFilter):
 
     def filter(self, message):
         try:
-            weni_keys = weni_words.keys()
+            weni_keys = 文爱关键词.keys()
             return any(key in message.text for key in weni_keys)
         except:
             return False
@@ -45,15 +45,15 @@ class FilterTextLen(MessageFilter):
             return False
 
 
-filter_setu = filters.Regex(regex_setu)
-filter_ohayo = filters.Regex(regex_ohayo) & FilterTextLen()
-filter_sleep = filters.Regex(regex_sleep) & FilterTextLen()
-filter_niubi = filters.Regex(regex_niubi)
-filter_yinyu = filters.Regex(regex_yinyu) & filters.Regex(
-    regex_noyinyu) & FilterTextLen(minlen=2, maxlen=10)
-filter_at = filters.Regex(regex_at)
-filter_weni = FilterWeniKey() & (~filters.Regex(regex_at)) & FilterTextLen(
+filter_setu = filters.Regex(色图正则)
+filter_ohayo = filters.Regex(早安正则) & FilterTextLen()
+filter_sleep = filters.Regex(晚安正则) & FilterTextLen()
+filter_niubi = filters.Regex(牛逼话正则)
+filter_yinyu = filters.Regex(淫语正则) & filters.Regex(
+    不要淫语正则) & FilterTextLen(minlen=2, maxlen=10)
+filter_at = filters.Regex(艾特正则)
+filter_weni = FilterWeniKey() & (~filters.Regex(艾特正则)) & FilterTextLen(
     minlen=1) & filters.ChatType.PRIVATE
-filter_mcmod = filters.Regex(regex_mcmod)
-filter_into_dict = (filters.Regex(regex_into_dict) &
+filter_mcmod = filters.Regex(mcmod正则)
+filter_into_dict = (filters.Regex(入典正则) &
                     FilterTextLen(minlen=1, maxlen=10))
