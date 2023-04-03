2f1203c8545053de4fe44eda67a98fc4d025bbc3
krau <krau39@outlook.com>
2023-04-03 14:39:03 +0800

    日志等级改为debug

--- src/words.py
+++ src/words.py
@@ -37,44 +37,44 @@ class GetWords():
     def get_aoligei(self) -> str:
         '''获取正能量'''
         word = random.choice(self.aoligei)
-        logger.info(f'获取到正能量:{word}')
+        logger.debug(f'获取到正能量:{word}')
         return word
 
     def get_niubi(self) -> str:
         '''获取装b句子'''
         word = str(random.choice(self.niubi)).replace('botname', botname)
-        logger.info(f'获取到niubi:{word}')
+        logger.debug(f'获取到niubi:{word}')
         return word
 
     def get_wanan(self) -> str:
         '''晚安!'''
         word = random.choice(self.wanan)
-        logger.info(f'获取到晚安:{word}')
+        logger.debug(f'获取到晚安:{word}')
         return word
 
     def get_young(self) -> str:
         '''获取大老师语录'''
         word = random.choice(self.young)
-        logger.info(f'获取到语录:{word}')
+        logger.debug(f'获取到语录:{word}')
         return word
 
     def get_ohayo(self) -> str:
         '''早安!'''
         word = random.choice(self.ohayo)
-        logger.info(f'获取到早安:{word}')
+        logger.debug(f'获取到早安:{word}')
         return word
 
     def get_yinyu(self, text) -> str:
         '''返回yinyu'''
         en = ''.join(re.findall(r'[a-zA-Z]', text))
         yinyu = dp(en.lower())
-        logger.info(f'获取到yinyu:{yinyu}')
+        logger.debug(f'获取到yinyu:{yinyu}')
         return yinyu
 
     def get_en(self, text: str) -> str:
         '''返回句中的英文'''
         en = ''.join(re.findall(r'[a-zA-Z]', text))
-        logger.info(f'匹配到英文字符{en}')
+        logger.debug(f'匹配到英文字符{en}')
         return en
 
     def get_weni(self, text: str) -> str:
@@ -88,7 +88,7 @@ class GetWords():
     def get_at_reply(self) -> str:
         '''有人叫我'''
         word = str(random.choice(self.at)).replace('botname', botname)
-        logger.info(f'获取到at_reply:{word}')
+        logger.debug(f'获取到at_reply:{word}')
         return word
 
     def get_mcmod_url(self, text: str) -> list[str]:
@@ -96,8 +96,8 @@ class GetWords():
         mod_nums = re.findall(r"www\.mcmod\.cn/class/([0-9]+)", text)
         urls = [f"https://www.mcmod.cn/class/{x}" for x in mod_nums]
         if urls:
-            logger.info(f'获取到mcmod链接{urls}')
+            logger.debug(f'获取到mcmod链接{urls}')
             return urls
         else:
-            logger.info('无mcmod链接')
+            logger.debug('无mcmod链接')
             return []
