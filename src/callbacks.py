b96ae9a96161e7bf68f509c37421bfb766cce1cb
krau <me@krau.top>
2023-04-24 23:55:45 +0800

    为其他人指定头衔

--- src/callbacks.py
+++ src/callbacks.py
@@ -96,44 +96,56 @@ async def title(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         logger.info(f"Bot: {sent_message.text}")
         return
-    if update.effective_message.text == "/t":
-        sent_message = await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.effective_message.id,
-            text="你要艾特Kmua!",
-        )
-        logger.info(f"Bot: {update.effective_message.text}")
-        return
+    this_user = update.effective_user
+    this_message = update.effective_message
+    replied_user = None
+    replied_message = None
     bot_username = context.bot_data["bot_username"]
-    custom_title = update.effective_message.text[len(bot_username) + 4 :]
+    custom_title = update.effective_message.text[3:]
+    user_id = this_user.id
+    if bot_username in this_message.text:
+        custom_title = custom_title.replace(bot_username, "")[1:]
+    if update.effective_message.reply_to_message:
+        replied_message = update.effective_message.reply_to_message
+        replied_user = replied_message.from_user
+        user_id = replied_user.id
+        if not custom_title:
+            custom_title = (
+                replied_user.username
+                if replied_user.username
+                else replied_user.full_name
+            )
     if not custom_title:
-        custom_title = (
-            update.effective_user.username
-            if update.effective_user.username
-            else update.effective_user.full_name
-        )
+        custom_title = this_user.username if this_user.username else this_user.full_name
     try:
         await context.bot.promote_chat_member(
             chat_id=update.effective_chat.id,
-            user_id=update.effective_user.id,
+            user_id=user_id,
             can_manage_chat=True,
             can_change_info=True,
             can_manage_video_chats=True,
             can_pin_messages=True,
             can_invite_users=True,
         )
-        logger.debug(
-            f"Bot将[{update.effective_chat.title}]({update.effective_user.name})设为管理员"
-        )
         await context.bot.set_chat_administrator_custom_title(
             chat_id=update.effective_chat.id,
-            user_id=update.effective_user.id,
+            user_id=user_id,
             custom_title=custom_title,
+        )
+        if replied_message:
+            text_when_have_replied_message = f"""
+            [{this_user.full_name}](tg://user?id={this_user.id})把[{replied_user.full_name}](tg://user?id={replied_user.id})变成{custom_title}!
+            """
+        text = (
+            f"好, 你现在是{custom_title}啦"
+            if not replied_message
+            else text_when_have_replied_message
         )
         sent_message = await context.bot.send_message(
             chat_id=update.effective_chat.id,
             reply_to_message_id=update.effective_message.id,
-            text=f"好, 你现在是{custom_title}啦",
+            text=text,
+            parse_mode="Markdown",
         )
         logger.info(f"Bot: {sent_message.text}")
     except BadRequest:
