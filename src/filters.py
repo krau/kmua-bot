d5ed01ae75478ff89e0b0becc1e67752be6f8ddf
krau <me@krau.top>
2023-04-24 15:13:54 +0800

    修复乱互动

--- src/filters.py
+++ src/filters.py
@@ -21,6 +21,8 @@ class interactFilter(MessageFilter):
         if not message.text:
             return False
         if message.text.startswith("/") or message.text.startswith("\\"):
+            if "bot_command" in message.entities[0].type:
+                return False
             return True
         return False
 
