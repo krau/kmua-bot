b8e87253561eb247359e0257061f4a546bda24d6
krau <me@krau.top>
2023-05-06 19:41:54 +0800

    增加用户词库目录

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,15 +1,19 @@
 import json
 import pathlib
 import glob
+import os
 from ..logger import logger
 
 
 def _load_word_dict():
-    word_dict_path = (
+    word_dict_path_internal = (
         pathlib.Path(__file__).parent.parent.parent / "resource" / "word_dicts"
     )
+    word_dict_path_user = (
+        pathlib.Path(__file__).parent.parent.parent / "data" / "word_dicts"
+    )
     word_dict = {}
-    for file in glob.glob(f"{word_dict_path}" + r"/*.json"):
+    for file in glob.glob(f"{word_dict_path_internal}" + r"/*.json"):
         try:
             with open(file, "r") as f:
                 for k, v in json.load(f).items():
@@ -20,6 +24,18 @@ def _load_word_dict():
         except Exception as e:
             logger.error(f"加载词库失败: {file}: {e.__class__.__name__}: {e}")
             continue
+    if os.path.exists(word_dict_path_user):
+        for file in glob.glob(f"{word_dict_path_user}" + r"/*.json"):
+            try:
+                with open(file, "r") as f:
+                    for k, v in json.load(f).items():
+                        if k in word_dict:
+                            word_dict[k].extend(v)
+                        else:
+                            word_dict[k] = v
+            except Exception as e:
+                logger.error(f"加载词库失败: {file}: {e.__class__.__name__}: {e}")
+                continue
     return word_dict
 
 
