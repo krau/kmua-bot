53512c2c507e7f16e614f3a37ac9109883c0641b
krau <me@krau.top>
2023-05-21 19:30:42 +0800

    feat: 互动支持chat身份

--- src/callbacks/interact.py
+++ src/callbacks/interact.py
@@ -15,41 +15,54 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await message_recorder(update, context)
     text = ""
     cmd = ""
-    message_text = update.effective_message.text
+    message = update.effective_message
     if reply_to_message := update.effective_message.reply_to_message:
         # 如果是对其他人使用
+        this_is_chat = True if message.sender_chat else False
+        replied_is_chat = True if reply_to_message.sender_chat else False
         this_user = update.effective_user
-        replied_user = reply_to_message.from_user
         this_name = this_user.full_name
-        replied_name = replied_user.full_name
         this_id = this_user.id
+        this_link = f"[{this_name}](tg://user?id={this_id})"
+        if this_is_chat:
+            this_user = message.sender_chat
+            this_name = this_user.title
+            this_id = this_user.username
+            this_link = f"[{this_name}](https://t.me/{this_id})"
+        replied_user = reply_to_message.from_user
+        replied_name = replied_user.full_name
         replied_id = replied_user.id
-        if len(message_text.split(" ")) == 1:
-            if message_text.startswith("/"):
-                cmd = message_text[1:].replace("/", "")
-                text = f"[{this_name}](tg://user?id={this_id}){cmd}了[{replied_name}](tg://user?id={replied_id})!"  # noqa: E501
-
-            elif message_text.startswith("\\"):
-                cmd = message_text[1:]
-                text = f"[{this_name}](tg://user?id={this_id})被[{replied_name}](tg://user?id={replied_id}){cmd}了!"  # noqa: E501
+        replied_link = f"[{replied_name}](tg://user?id={replied_id})"
+        if replied_is_chat:
+            replied_user = reply_to_message.sender_chat
+            replied_name = replied_user.title
+            replied_id = replied_user.username
+            replied_link = f"[{replied_name}](https://t.me/{replied_id})"
+        if len(message.text.split(" ")) == 1:
+            if message.text.startswith("/"):
+                cmd = message.text[1:].replace("/", "")
+                text = f"{this_link}{cmd}了{replied_link}!"
+            elif message.text.startswith("\\"):
+                cmd = message.text[1:]
+                text = f"{this_link}被{replied_link}{cmd}了!"
         else:
-            if message_text.startswith("/"):
-                cmd_front = message_text.split(" ")[0].replace("/", "")
-                cmd_back = message_text.split(" ")[1:]
+            if message.text.startswith("/"):
+                cmd_front = message.text.split(" ")[0].replace("/", "")
+                cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back).replace("/", "")
-                text = f"[{this_name}](tg://user?id={this_id}){cmd_front}[{replied_name}](tg://user?id={replied_id}){cmd_back}!"  # noqa: E501
-            elif message_text.startswith("\\"):
-                cmd_front = message_text.split(" ")[0].replace("\\", "")
-                cmd_back = message_text.split(" ")[1:]
+                text = f"{this_link}{cmd_front}{replied_link}{cmd_back}!"
+            elif message.text.startswith("\\"):
+                cmd_front = message.text.split(" ")[0].replace("\\", "")
+                cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back)
-                text = f"[{replied_name}](tg://user?id={replied_id}){cmd_front}[{this_name}](tg://user?id={this_id}){cmd_back}!"  # noqa: E501
+                text = f"{replied_link}{cmd_front}{this_link}{cmd_back}!"
     else:
         # 如果是对自己使用
-        if message_text.startswith("/"):
-            cmd = message_text[1:].replace("/", "")
+        if message.text.startswith("/"):
+            cmd = message.text[1:].replace("/", "")
             text = f"[{update.effective_user.full_name}](tg://user?id={update.effective_user.id}){cmd}了自己!"  # noqa: E501
-        elif message_text.startswith("\\"):
-            cmd = message_text[1:]
+        elif message.text.startswith("\\"):
+            cmd = message.text[1:]
             text = f"[{update.effective_user.full_name}](tg://user?id={update.effective_user.id})被自己{cmd}了!"  # noqa: E501
     sent_message = await context.bot.send_message(
         chat_id=update.effective_chat.id, text=text, parse_mode="Markdown"
