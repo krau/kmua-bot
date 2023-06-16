218899c0c41d6195f5e6bf7c18fab53e60f0599e
krau <me@krau.top>
2023-06-16 08:30:46 +0800

    use MarkdownV2

--- src/callbacks/interact.py
+++ src/callbacks/interact.py
@@ -20,7 +20,7 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     this_user = update.effective_user
     if this_is_chat:
         this_user = message.sender_chat
-    this_link = this_user.mention_markdown()
+    this_link = this_user.mention_markdown_v2()
     if reply_to_message := update.effective_message.reply_to_message:
         # 如果是对其他人使用
         replied_is_chat = True if reply_to_message.sender_chat else False
@@ -28,46 +28,46 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
             replied_user = reply_to_message.sender_chat
         else:
             replied_user = reply_to_message.from_user
-        replied_link = replied_user.mention_markdown()
+        replied_link = replied_user.mention_markdown_v2()
         if len(message.text.split(" ")) == 1:
             if message.text.startswith("/"):
                 cmd = message.text[1:].replace("/", "")
-                text = f"{this_link}{cmd}了{replied_link}!"
+                text = f"{this_link}{cmd}了{replied_link} \!"
             elif message.text.startswith("\\"):
                 cmd = message.text[1:]
-                text = f"{this_link}被{replied_link}{cmd}了!"
+                text = f"{this_link}被{replied_link}{cmd}了 \!"
         else:
             if message.text.startswith("/"):
                 cmd_front = message.text.split(" ")[0].replace("/", "")
                 cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back).replace("/", "")
-                text = f"{this_link}{cmd_front}{replied_link}{cmd_back}!"
+                text = f"{this_link}{cmd_front}{replied_link}{cmd_back} \!"
             elif message.text.startswith("\\"):
                 cmd_front = message.text.split(" ")[0].replace("\\", "")
                 cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back)
-                text = f"{replied_link}{cmd_front}{this_link}{cmd_back}!"
+                text = f"{replied_link}{cmd_front}{this_link}{cmd_back} \!"
     else:
         # 如果是对自己使用
         if len(message.text.split(" ")) == 1:
             if message.text.startswith("/"):
                 cmd = message.text[1:].replace("/", "")
-                text = f"{this_link}{cmd}了!"
+                text = f"{this_link}{cmd}了\!"
             elif message.text.startswith("\\"):
                 cmd = message.text[1:]
-                text = f"{this_link}被自己{cmd}了!"
+                text = f"{this_link}被自己{cmd}了 \!"
         else:
             if message.text.startswith("/"):
                 cmd_front = message.text.split(" ")[0].replace("/", "")
                 cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back).replace("/", "")
-                text = f"{this_link}{cmd_front}自己{cmd_back}!"
+                text = f"{this_link}{cmd_front}自己{cmd_back} \!"
             elif message.text.startswith("\\"):
                 cmd_front = message.text.split(" ")[0].replace("\\", "")
                 cmd_back = message.text.split(" ")[1:]
                 cmd_back = " ".join(cmd_back)
-                text = f"{this_link}被自己{cmd_front}了{cmd_back}!"
+                text = f"{this_link}被自己{cmd_front}了{cmd_back} \!"
     sent_message = await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=text, parse_mode="Markdown"
+        chat_id=update.effective_chat.id, text=text, parse_mode="MarkdownV2"
     )
     logger.info(f"Bot: {sent_message.text}")
