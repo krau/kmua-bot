071e868b989fa5e6811c3414dab6ea75cd3e90a8
krau <me@krau.top>
2023-06-25 18:49:55 +0800

    简化1下; 处理用户销号时被抽到的情况

--- src/callbacks/waifu.py
+++ src/callbacks/waifu.py
@@ -25,24 +25,19 @@ async def today_waifu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not waifu_id:
         is_got_waifu = False
         group_member: list[int] = list(context.chat_data["members_data"].keys())
-        group_member.remove(user_id)
-        if 777000 in group_member:
-            group_member.remove(777000)
-        if 136817688 in group_member:
-            group_member.remove(136817688)
-        if 1087968824 in group_member:
-            group_member.remove(1087968824)
+        to_remove = [user_id, 777000, 136817688, 1087968824, context.bot.id]
+        group_member = [member for member in group_member if member not in to_remove]
         if not group_member:
-            await update.message.reply_text(text="你现在没有老婆, 因为kmua的记录中找不到其他群友")  # noqa: E501
+            await update.message.reply_text(text="你现在没有老婆, 因为kmua的记录中找不到其他群友")
             return
         waifu_id = random.choice(group_member)
     try:
         waifu = await context.bot.get_chat(waifu_id)
         is_success = True
     except BadRequest:
-        logger.warning(f"无法为 {update.effective_user.name} 获取id为 {waifu_id} 的waifu")  # noqa: E501
+        logger.warning(f"无法为 {update.effective_user.name} 获取id为 {waifu_id} 的waifu")
         await update.message.reply_text(text="你没能抽到老婆, 再试一次吧~")
-        poped_value = context.chat_data["members_data"].pop(waifu_id, "群组数据中无该成员")  # noqa: E501
+        poped_value = context.chat_data["members_data"].pop(waifu_id, "群组数据中无该成员")
         logger.debug(f"移除: {poped_value}")
         return
     except Exception as e:
@@ -51,10 +46,20 @@ async def today_waifu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if avatar:
         avatar = await (await waifu.photo.get_big_file()).download_as_bytearray()
         avatar = bytes(avatar)
-    if is_got_waifu:
-        text = f"你今天已经抽过老婆了\! {waifu.mention_markdown_v2()} 是你今天的老婆\!"
-    else:
-        text = f"你今天的群友老婆是 {waifu.mention_markdown_v2()} \!"
+    try:
+        if is_got_waifu:
+            text = f"你今天已经抽过老婆了\! {waifu.mention_markdown_v2()} 是你今天的老婆\!"
+        else:
+            text = f"你今天的群友老婆是 {waifu.mention_markdown_v2()} \!"
+    except TypeError:
+        if is_got_waifu:
+            text = f"你的老婆消失了...TA的id曾是: *{waifu_id}*"
+        else:
+            text = "你没能抽到老婆, 再试一次吧~"
+        await update.message.reply_text(text=text, parse_mode="MarkdownV2")
+        poped_value = context.chat_data["members_data"].pop(waifu_id, "群组数据中无该成员")
+        logger.debug(f"移除: {poped_value}")
+        return
     if avatar:
         await update.message.reply_photo(
             photo=avatar,
