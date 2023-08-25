753e3f079bb17564ed45fa550ee53f8ff97df916
krau <me@krau.top>
2023-08-25 09:53:56 +0800

    enhance: use markdown v2

--- src/callbacks/interact.py
+++ src/callbacks/interact.py
@@ -24,10 +24,10 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
     this_user = message.sender_chat if message.sender_chat else message.from_user
     try:
-        this_mention = this_user.mention_markdown()
+        this_mention = this_user.mention_markdown_v2()
     except TypeError:
         this_mention = (
-            f"[{escape_markdown(this_user.title)}](tg://user?id={this_user.id})"
+            f"[{escape_markdown(this_user.title,2)}](tg://user?id={this_user.id})"
         )
     replied_user = None
     replied_mention = ""
@@ -38,9 +38,9 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
             else reply_to_message.from_user
         )
         try:
-            replied_mention = replied_user.mention_markdown()
+            replied_mention = replied_user.mention_markdown_v2()
         except TypeError:
-            replied_mention = f"[{escape_markdown(replied_user.title)}](tg://user?id={replied_user.id})"
+            replied_mention = f"[{escape_markdown(replied_user.title,2)}](tg://user?id={replied_user.id})"
     is_backslash = (
         False
         if message.text.startswith("/")
@@ -51,36 +51,36 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if is_backslash is None:
         return
     is_one_cmd = True if len(message.text.split(" ")) == 1 else False
-    cmd1 = escape_markdown(replace_special_char(message.text.split(" ")[0][1:]))
+    cmd1 = escape_markdown(replace_special_char(message.text.split(" ")[0][1:]), 2)
     if not is_one_cmd:
         cmd2 = escape_markdown(
-            replace_special_char(" ".join(message.text.split(" ")[1:]))
+            replace_special_char(" ".join(message.text.split(" ")[1:])), 2
         )
         text = (
             (
-                f"{this_mention} 被 {replied_mention} {cmd1}{cmd2} !"
+                f"{this_mention} 被 {replied_mention} {cmd1}{cmd2} \!"
                 if replied_user
-                else f"{this_mention} 被自己{cmd1}{cmd2} !"
+                else f"{this_mention} 被自己{cmd1}{cmd2} \!"
             )
             if is_backslash
             else (
-                f"{this_mention} {cmd1} {replied_mention} {cmd2} !"
+                f"{this_mention} {cmd1} {replied_mention} {cmd2} \!"
                 if replied_user
-                else f"{this_mention} {cmd1}自己{cmd2} !"
+                else f"{this_mention} {cmd1}自己{cmd2} \!"
             )
         )
     else:
         text = (
             (
-                f"{this_mention} 被 {replied_mention} {cmd1}了 !"
+                f"{this_mention} 被 {replied_mention} {cmd1}了 \!"
                 if replied_user
-                else f"{this_mention} 被自己{cmd1}了 !"
+                else f"{this_mention} 被自己{cmd1}了 \!"
             )
             if is_backslash
             else (
-                f"{this_mention} {cmd1}了 {replied_mention} !"
+                f"{this_mention} {cmd1}了 {replied_mention} \!"
                 if replied_user
-                else f"{this_mention} {cmd1}了自己 !"
+                else f"{this_mention} {cmd1}了自己 \!"
             )
         )
     # 在中英文之间加空格
@@ -89,6 +89,6 @@ async def interact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text,
-        parse_mode="Markdown",
+        parse_mode="MarkdownV2",
     )
     await message_recorder(update, context)
