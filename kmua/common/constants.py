a14d4a29bfc7e9656d75bb23f30724dd0398d2b3
krau <71133316+krau@users.noreply.github.com>
2024-07-03 21:50:04 +0800

    更改帮助页面

--- kmua/common/constants.py
+++ kmua/common/constants.py
@@ -10,7 +10,7 @@ from kmua.config import data_dir, settings
 QUOTE_PAGE_SIZE = 5
 PAGE_JUMP_SIZE = 20
 
-DETAIL_HELP_URL = "https://krau.github.io/kmua-bot/"
+DETAIL_HELP_URL = "https://kmua.unv.app"
 OPEN_SOURCE_URL = "https://github.com/krau/kmua-bot/"
 
 BACK_HOME_MARKUP = InlineKeyboardMarkup(
