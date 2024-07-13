7516399dcb51b9a72e6327aec9440083c3fba5c1
krau <71133316+krau@users.noreply.github.com>
2024-07-14 00:09:42 +0800

    fix database default path

--- kmua/common/constants.py
+++ kmua/common/constants.py
@@ -22,7 +22,7 @@ BACK_HOME_MARKUP = InlineKeyboardMarkup(
 )
 DB_PATH = (
     Path(data_dir / settings.get("db_url", "sqlite:///./data/kmua.db").split("/")[-1])
-    if settings.get("db_url").startswith("sqlite")
+    if settings.get("db_url", "sqlite:///./data/kmua.db").startswith("sqlite")
     else None
 )
 DEFAULT_BIG_AVATAR_PATH = (
