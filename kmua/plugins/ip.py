433ac5f930474b0cd10e6a9d7059a35766c5ead1
krau <71133316+krau@users.noreply.github.com>
2025-05-26 21:57:20 +0800

    feat: ip

---
+++ kmua/plugins/ip.py
@@ -0,0 +1,125 @@
+import contextlib
+from urllib.parse import urlparse
+
+import httpx
+from pyrogram import Client, filters
+from pyrogram.enums import ChatType, ParseMode
+from pyrogram.types import Message
+
+from kmua import common, database, i18n
+from kmua.logger import logger
+
+
+@Client.on_message(filters.command("ip"), group=0)
+async def ipinfo(client: Client, message: Message):
+    user = message.from_user
+
+    chat = message.chat
+    in_group = chat.type in (ChatType.GROUP, ChatType.SUPERGROUP)
+    if in_group:
+        lang = (await database.get_chat_config(chat.id)).lang
+    else:
+        lang = (await database.get_user_config(user.id)).lang
+
+    ip = None
+
+    if message.reply_to_message:
+        ip = message.reply_to_message.text or message.reply_to_message.caption
+
+    if not ip and message.command and len(message.command) > 1:
+        ip = message.command[1]
+
+    if not ip:
+        await message.reply_text(i18n.t("bot.msg.ip.no_ip_provided", locale=lang))
+        return
+
+    ip_parsed = urlparse(ip)
+    ip = ip_parsed.hostname or ip_parsed.path or ip_parsed.netloc
+
+    if not ip:
+        await message.reply_text(i18n.t("bot.msg.ip.no_ip_provided", locale=lang))
+        return
+
+    sent_message = await message.reply_text(
+        i18n.t("bot.msg.ip.querying", locale=lang).format(ip=ip)
+    )
+
+    querying_key = f"ip_querying:{user.id}"
+    if await common.memstore.get(querying_key):
+        return
+    try:
+        await common.memstore.set(querying_key, True)
+        result = await _get_ip_info(ip, lang)
+        await sent_message.edit_text(text=result, parse_mode=ParseMode.HTML)
+    except Exception as e:
+        logger.warning(f"{e.__class__.__name__}: {e}")
+        await sent_message.edit_text(
+            i18n.t("bot.msg.ip.query_failed", locale=lang).format(
+                error_type=e.__class__.__name__, error=str(e)
+            )
+        )
+    finally:
+        await common.memstore.delete(querying_key)
+
+
+async def _get_ip_info(url: str, lang: str) -> str:
+    async with httpx.AsyncClient() as client:
+        data = await client.get(
+            url="http://ip-api.com/json/" + url,
+            params={
+                "fields": "status,message,country,regionName,"
+                "city,lat,lon,isp,org,as,mobile,proxy,hosting,query"
+            },
+        )
+    ipinfo_json = data.json()
+    if ipinfo_json["status"] == "fail":
+        return i18n.t("bot.msg.ip.query_failed_reason", locale=lang).format(
+            message=ipinfo_json["message"]
+        )
+    if ipinfo_json["status"] == "success":
+        ipinfo_list = [i18n.t("bot.msg.ip.query_target", locale=lang).format(url=url)]
+        if ipinfo_json["query"] != url:
+            ipinfo_list.extend(
+                [
+                    i18n.t("bot.msg.ip.resolved_address", locale=lang).format(
+                        query=ipinfo_json["query"]
+                    )
+                ]
+            )
+        ipinfo_list.extend(
+            [
+                i18n.t("bot.msg.ip.location", locale=lang).format(
+                    country=ipinfo_json["country"],
+                    region=ipinfo_json["regionName"],
+                    city=ipinfo_json["city"],
+                ),
+                i18n.t("bot.msg.ip.coordinates", locale=lang).format(
+                    lat=str(ipinfo_json["lat"]), lon=str(ipinfo_json["lon"])
+                ),
+                i18n.t("bot.msg.ip.isp", locale=lang).format(isp=ipinfo_json["isp"]),
+            ]
+        )
+        if ipinfo_json["org"] != "":
+            ipinfo_list.extend(
+                [
+                    i18n.t("bot.msg.ip.organization", locale=lang).format(
+                        org=ipinfo_json["org"]
+                    )
+                ]
+            )
+        with contextlib.suppress(Exception):
+            ipinfo_list.extend(
+                [
+                    i18n.t("bot.msg.ip.as_number", locale=lang).format(
+                        as_number=ipinfo_json["as"],
+                        as_link=ipinfo_json["as"].split()[0],
+                    )
+                ]
+            )
+        if ipinfo_json["mobile"]:
+            ipinfo_list.extend([i18n.t("bot.msg.ip.mobile_ip", locale=lang)])
+        if ipinfo_json["proxy"]:
+            ipinfo_list.extend([i18n.t("bot.msg.ip.proxy_ip", locale=lang)])
+        if ipinfo_json["hosting"]:
+            ipinfo_list.extend([i18n.t("bot.msg.ip.hosting_ip", locale=lang)])
+        return "\n".join(ipinfo_list)
