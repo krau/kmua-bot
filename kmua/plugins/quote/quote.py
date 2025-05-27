4c88598db67226465d5b4a493bb9b4e410b4cff7
krau <71133316+krau@users.noreply.github.com>
2025-05-27 23:32:31 +0800

    fix: remove unused button text logic in inline_quote_query function

--- kmua/plugins/quote/quote.py
+++ kmua/plugins/quote/quote.py
@@ -321,28 +321,34 @@ async def inline_quote_query(
     query.offset
     quotes = await database.take_quotes_user_can_see(user.id, data, 50)
     for quote_db in quotes:
-        button_text = (
-            quote_db.user.full_name
-            if len(quote_db.user.full_name) <= 16
-            else quote_db.user.full_name[:16] + "..."
-            if quote_db.user.full_name
-            else str(quote_db.user_id)
+        # button_text = (
+        #     quote_db.user.full_name
+        #     if len(quote_db.user.full_name) <= 16
+        #     else quote_db.user.full_name[:16] + "..."
+        #     if quote_db.user.full_name
+        #     else str(quote_db.user_id)
+        # )
+        # markup = pyrogram.types.InlineKeyboardMarkup(
+        #     [
+        #         [
+        #             pyrogram.types.InlineKeyboardButton(
+        #                 text=button_text,
+        #                 url=quote_db.link,
+        #             )
+        #         ]
+        #     ]
+        # )
+
+        # https://github.com/krau/kmua-bot/issues/71
+        markup = pyrogram.types.InlineKeyboardMarkup(
+            [[pyrogram.types.InlineKeyboardButton(text="↗️", url=quote_db.link)]]
         )
         if quote_db.img:
             results.append(
                 pyrogram.types.InlineQueryResultCachedPhoto(
                     quote_db.img,
                     title=quote_db.text or "",
-                    reply_markup=pyrogram.types.InlineKeyboardMarkup(
-                        [
-                            [
-                                pyrogram.types.InlineKeyboardButton(
-                                    text=button_text,
-                                    url=quote_db.link,
-                                )
-                            ]
-                        ]
-                    ),
+                    reply_markup=markup,
                 )
             )
         results.append(
@@ -351,16 +357,7 @@ async def inline_quote_query(
                 input_message_content=pyrogram.types.InputTextMessageContent(
                     message_text=quote_db.text or "",
                 ),
-                reply_markup=pyrogram.types.InlineKeyboardMarkup(
-                    [
-                        [
-                            pyrogram.types.InlineKeyboardButton(
-                                text=button_text,
-                                url=quote_db.link,
-                            )
-                        ]
-                    ]
-                ),
+                reply_markup=markup,
             )
         )
         if len(results) >= 50:
