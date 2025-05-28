e6e729288762fd63ac155dc75e508c2174cd4e09
krau <71133316+krau@users.noreply.github.com>
2025-05-28 12:15:10 +0800

    fix: 优化图像处理逻辑，提取图像缩放功能至独立函数并使用异步处理

--- kmua/plugins/manyacg/utils.py
+++ kmua/plugins/manyacg/utils.py
@@ -1,9 +1,27 @@
+import asyncio
 import io
 
 import httpx
 from PIL import Image
 
 from kmua import common
+
+_max_size = 2560
+
+
+def _resize_image(pic_bytes: bytes) -> bytes:
+    with Image.open(io.BytesIO(pic_bytes)) as image:
+        ratio = _max_size / max(image.width, image.height)
+
+        if ratio < 1:
+            new_size = (int(image.width * ratio), int(image.height * ratio))
+            image = image.resize(new_size, Image.Resampling.LANCZOS)
+
+        output = io.BytesIO()
+        image.convert("RGB").save(output, format="JPEG", quality=90)
+        processed_bytes = output.getvalue()
+        output.close()
+        return processed_bytes
 
 
 async def prepare_media(
@@ -14,24 +32,16 @@ async def prepare_media(
 
     cache: str | None = await common.memttlcache.get(f"artwork:pic_file_id:{image_url}")
     if cache is not None:
-        print(f"Using cached file_id for {image_url}")
         return cache
 
     pic_bytes: bytes = await (await client.get(image_url)).aread()
 
-    # resize image if size > 10M or exceeds width/height limitation
     if (
         len(pic_bytes) > 1024 * 1024 * 10
         or picture["width"] + picture["height"] > 10000
     ):
-        image = Image.open(io.BytesIO(pic_bytes))
-        max_size = 2560
-        ratio = max_size / max(image.width, image.height)
-        if ratio < 1:
-            new_size = (int(image.width * ratio), int(image.height * ratio))
-            image = image.resize(new_size, Image.Resampling.LANCZOS)
-        output = io.BytesIO()
-        image.convert("RGB").save(output, format="JPEG", quality=90)
-        pic_bytes = output.getvalue()
-        output.close()
+        pic_bytes = await asyncio.to_thread(
+            _resize_image, pic_bytes, picture["width"], picture["height"]
+        )
+
     return pic_bytes
