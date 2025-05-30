57c35fba1a89a583417b3e56bfa4e6581b12474e
krau <71133316+krau@users.noreply.github.com>
2025-05-30 15:17:30 +0800

    refacot reply module

--- kmua/plugins/reply/reply.py
+++ kmua/plugins/reply/reply.py
@@ -6,53 +6,12 @@ from pyrogram import filters
 
 from kmua import database, i18n, resources
 
-
-async def base_filter_func(_, __, message: pyrogram.types.Message) -> bool:
-    if not message.text:
-        return False
-    if len(message.text) <= 1 or len(message.text) > 1024:
-        return False
-    if (
-        message.entities is not None
-        and message.entities[0].type == pyrogram.enums.MessageEntityType.BOT_COMMAND
-    ):
-        return False
-    if message.text.startswith("/") or message.text.startswith("\\"):
-        return False
-    return True
-
-
-async def reply_me_filter_func(
-    _, client: pyrogram.Client, message: pyrogram.types.Message
-) -> bool:
-    if not message.reply_to_message:
-        return False
-    if not message.reply_to_message.from_user:
-        return False
-    if not client.me:
-        return False
-    if message.reply_to_message.from_user.username != client.me.username:
-        return False
-    return True
-
-
-async def mention_me_filter_func(
-    _, client: pyrogram.Client, message: pyrogram.types.Message
-) -> bool:
-    if not message.text:
-        return False
-    if client.me.username in message.text:
-        return True
-    return False
-
-
-base_filter = filters.create(base_filter_func)
-reply_me_filter = filters.create(reply_me_filter_func)
-mention_me_filter = filters.create(mention_me_filter_func)
+from . import myfilter
 
 
 @pyrogram.Client.on_message(
-    base_filter & (reply_me_filter | filters.private | mention_me_filter),
+    myfilter.base_filter
+    & (myfilter.reply_me_filter | filters.private | myfilter.mention_me_filter),
     group=0,
 )
 async def word_reply(client: pyrogram.Client, message: pyrogram.types.Message):
