a641d5fdb9179551dfd5cfd7d28ea3ae35c1a0b7
krau <71133316+krau@users.noreply.github.com>
2025-06-02 14:18:36 +0800

    feat: 更新处理贴纸的逻辑，支持根据 MIME 类型动态设置文件扩展名并缓存文件 ID

--- kmua/plugins/tools/stickers.py
+++ kmua/plugins/tools/stickers.py
@@ -1,7 +1,10 @@
+import mimetypes
 from io import BytesIO
 from typing import BinaryIO
 
 import pyrogram
+
+from kmua import common
 
 
 @pyrogram.Client.on_message(
@@ -11,12 +14,30 @@ async def handle_sticker(client: pyrogram.Client, message: pyrogram.types.Messag
     sticker = message.sticker
     if not sticker:
         return
+    ext = mimetypes.guess_extension(sticker.mime_type) or ".png"
+    if ext == ".webp":
+        ext = ".png"
+    if file_id := await common.memttlcache.get(
+        f"sticker_file:{sticker.file_unique_id}"
+    ):
+        await message.reply_document(
+            document=file_id,
+            force_document=True,
+            file_name=f"{sticker.set_name}_{sticker.file_unique_id}.{ext}",
+        )
+        return
     await message.reply_chat_action(pyrogram.enums.ChatAction.UPLOAD_DOCUMENT)
     file = await client.download_media(sticker, in_memory=True)
     if not file or not isinstance(file, (str, BinaryIO, BytesIO)):
         return
-    await message.reply_document(
+    msg = await message.reply_document(
         document=file,
         force_document=True,
-        file_name=f"{sticker.set_name}_{sticker.file_unique_id}.png",
+        file_name=f"{sticker.set_name}_{sticker.file_unique_id}.{ext}",
     )
+    if msg.document and msg.document.file_id:
+        await common.memttlcache.set(
+            f"sticker_file:{sticker.file_unique_id}",
+            msg.document.file_id,
+            ttl=86400 * 3,
+        )
